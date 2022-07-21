b2db6de06e5c4a4e10ba1deab2d79985c39c1743
Margarita Iglesias <margaritaigle99@gmail.com>
2022-07-20 22:53:46 -0300

    can add categories in app

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,14 +1,35 @@
-
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+
+  const onAddCategory = ( newCategory ) => {
+    // setCategories( cat => [ ...cat, "Valorant" ] );
+    setCategories([newCategory, ...categories]);
+  }
+
   return (
     <>
       {/* {titulo} */}
       <h1>GifExpertApp</h1>
+
       {/* { Input } */}
-      
-      {/* {Listado de gif} */}  
-        {/* {Gif Item} */}
+      <AddCategory
+        // addNewCategory={setCategories}
+        onNewCategory={ (value) => onAddCategory(value) }
+      />
+
+      {/* {Listado de gif} */}
+      <ol>
+        {categories.map((category) => {
+          return <li key={ category } >{ category }</li>
+        })}
+
+      </ol>
+      {/* {Gif Item} */}
     </>
   );
 }
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState('')
+
+    const onInputChange = ( event ) => {
+        setInputValue(event.target.value)
+    }
+
+    // const onInputChange = ( { target } ) => {
+    //     setInputValue(target.value)
+    // } con desestructuracion
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if( inputValue.trim().length <= 1 ) return;
+
+        // addNewCategory( (categories) => [ inputValue, ...categories ] );
+        onNewCategory( inputValue.trim() );
+        setInputValue('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Search gifs"
+                value={inputValue}
+                // onChange={ (event) => onInputChange(event) }
+                onChange={onInputChange}
+            ></input>
+        </form>
+    )
+}
