8dff5b0d29fe3262b6d822ba9f8fae48064a0edc
Margarita Iglesias <margaritaigle99@gmail.com>
2022-07-23 21:26:36 -0300

    almost finished

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,15 +1,34 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import { getGifs } from '../helpers/getGifs'
+import { GifItem } from './GifItem.jsx';
 
 export const GifGrid = ( { category } ) => {
+
+    const [images, setImages] = useState([]);
+
+    const getImages = async () => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    }
+    
     useEffect(() => {
-        getGifs(category);
-   }, [])
+        getImages();
+    }, []);
   
 
   return (
       <>
           <h3>{category}</h3>
+          <div className='card-grid'>
+              {
+                  images.map((image) => (
+                      <GifItem 
+                        key={ image.id }
+                        { ...image }
+                      />
+                  ))
+              }
+          </div>
       </>
   )
 }
