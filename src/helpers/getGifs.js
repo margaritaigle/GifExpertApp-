f155a70c4f9cabf4231e4219e6f02d2109f03ce3
Margarita Iglesias <margaritaigle99@gmail.com>
2022-07-25 22:29:41 -0300

    ff

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,6 @@ export const getGifs = async ( category ) => {
     const url = `https://api.giphy.com/v1/gifs/search?api_key=QyN5OIn3NCXUH58sDza7Hb62Nz0DfrWO&q=${ category }&limit=10`
     const resp = await fetch( url );
     const { data } = await resp.json();
-    console.log(data);
     
     const gifs = data.map( ({id, title, images}) => ({
         id: id,
